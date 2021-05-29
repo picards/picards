921c338942e54a5cef1124e3be2f8cf12b0e1a7e
Michael <roy.weden@yahoo.com>
2021-05-29 14:31:29 -0500

    Basic flask webapp with heroku

---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
+from app import app
+
+@app.route('/')
+def home():
+    return "hello worlds"

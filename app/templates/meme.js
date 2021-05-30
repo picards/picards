367e963b30f83a129428e9dcc1df850933b8b398
hithaaaa <83871892+hithaaaa@users.noreply.github.com>
2021-05-30 17:03:07 -0500

    memessssssss

---
+++ app/templates/meme.js
@@ -0,0 +1,31 @@
+// Get all the images
+image_array = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+  '10.jpg',
+  '11.jpg',
+  '12.jpg',
+  '13.jpg',
+  '14.jpg',
+  '15.jpg',
+  
+
+]
+
+function get_random_image(){
+  // Get a random index
+  random_index = Math.floor(Math.random() * image_array.length);
+
+  // Get an image at the random_index
+  selected_image = image_array[random_index]
+
+  // Display the image
+  document.getElementById('image_shower').src = `./images/${selected_image}`
+}

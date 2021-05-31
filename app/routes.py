f3c4d8ccac4b0af95d1d2cf3708a227d20e0e466
Michael <roy.weden@yahoo.com>
2021-05-30 20:10:34 -0500

    Updated all

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,14 @@ import requests
 from app import app
 from sawo import createTemplate, verifyToken
 import json
+#from app.sqldb import print_accounts, create_account, create_card
 
 createTemplate("app/templates/partials", flask=True)
 
 load = ''
 loaded = 0
+account_id = ''
+email = ''
 
 def setPayload(payload):
     global load
@@ -23,40 +26,51 @@ def setLoaded(reset=False):
 
 @app.route('/')
 def landing():
-    return render_template("landing.html")
+    return render_template("landing.html", title="")
 
 @app.route('/addcards')
 def addcards():
-    return render_template("add_cards.html")
+    return render_template("add_cards.html", title="Add Card")
 
 @app.route('/sharecards')
 def sharecards():
-    return render_template("share.html")
+    return render_template("share.html", title="Share Cards")
 
 @app.route('/viewshared')
 def viewshared():
-    return render_template("add_cards.html")
+    return render_template("add_cards.html", title="View Shared")
 
 @app.route('/playcards')
 def playcards():
-    return render_template("play_cards.html")
+    return render_template("play_cards.html", title="Play Cards")
 
 @app.route('/home')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
+
+@app.route('/addcard', methods=["POST"])
+def addcard():
+    question = request.form['qn']
+    answer = request.form['ans']
+    #create_card(account_id, question, answer)
+    return redirect('/0/addcards')
 
 @app.route('/0/<name>')
 def open_page(name):
     if load != '':
+        account_id = load['user_id']
+        email = load['identifier']
+        #create_account(account_id, email)
+        #print_accounts()
         return redirect(url_for(name))
     setLoaded()
     setPayload(load if loaded < 2 else '')
     sawo = {
         "auth_key":"34b10c22-1add-4f46-90fa-8b28dbe38d83",
-        "to":("login"),
+        "to":"login",
         "identifier":"email"
     }
-    return render_template("sawo.html", sawo=sawo, load=load)
+    return render_template("sawo.html", sawo=sawo, load=load, title="Login")
 
 @app.route("/login", methods=["POST","GET"])
 def login():
--- app/sqldb.py
+++ app/sqldb.py
@@ -2,6 +2,7 @@ import time
 import random
 import logging
 from argparse import ArgumentParser, RawTextHelpFormatter
+import uuid
 
 import psycopg2
 from psycopg2.errors import SerializationFailure
@@ -12,12 +13,11 @@ conn = psycopg2.connect(
     user='michael',
     password='3H$xlwl1riDrupan4p7#',
     sslmode='verify-full',
-    sslrootcert='../cockroach/ca.crt',
+    sslrootcert='app/ca.crt',
     port=26257,
     host='free-tier.gcp-us-central1.cockroachlabs.cloud',
-    options="--cluster=pure-bull"
+    options="--cluster=pure-bull-2141"
 )
-
 
 # Make each statement commit immediately.
 conn.set_session(autocommit=True)
@@ -25,3 +25,33 @@ conn.set_session(autocommit=True)
 # Open a cursor to perform database operations.
 cur = conn.cursor()
 
+# Create the "accounts" table
+cur.execute("""CREATE TABLE IF NOT EXISTS accounts (
+    uuid UUID,
+    email STRING,
+    card_uuids UUID[]
+);""")
+
+# Create the "cards" table
+cur.execute("""CREATE TABLE IF NOT EXISTS cards (
+    uuid UUID,
+    question STRING,
+    answer STRING
+);""")
+
+def print_accounts():
+    cur.execute("SELECT * FROM accounts;")
+    for record in cur:
+        print(record)
+
+def create_account(uuid, email):
+    cur.execute("""INSERT INTO accounts (uuid, email)
+     VALUES ('{0}', '{1}');""".format(uuid, email))
+
+def create_card(account_uuid, question, answer):
+    card_uuid = str(uuid.uuid4())
+    cur.execute("SELECT uuid, card_uuids FROM accounts WHERE uuid = {}".format(account_uuid))
+    card_uuids = [cur.fetchone()[1], card_uuid]
+    cur.execute("""INSERT INTO cards (uuid, question, answer)
+     VALUES ('{0}', '{1}', '{2}');""".format(card_uuid, question, answer))
+    cur.execute("""UPDATE accounts SET card_uuids = '{0}' WHERE uuid = '{1}';""".format(card_uuids, account_uuid))
